dc096114e18a27c51831ab2bd2612fd48c330ab2
JeezLee <450994392@qq.com>
2017-10-20 10:00:09 -0500

    add taobao mirrors

--- nodejs/path.zsh
+++ nodejs/path.zsh
@@ -5,3 +5,6 @@ export NODEJS_ORG_MIRROR='https://npm.taobao.org/mirrors/node'
 export PHANTOMJS_CDNURL='https://npm.taobao.org/dist/phantomjs'
 export SASS_BINARY_SITE='https://npm.taobao.org/mirrors/node-sass'
 export ELECTRON_MIRROR='https://npm.taobao.org/mirrors/electron/'
+
+# use taobao mirrors
+# npm config set registry https://registry.npm.taobao.org
